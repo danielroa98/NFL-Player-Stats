de38e9090b6a20718a144149560bf4016ffd3538
Daniel Roa <daniel.roa98@gmail.com>
2022-12-15 15:44:14 -0600

    Implemented plots
    - Added Bargraph for the NFL.
    - Added player scatterplot to better compare them.

--- pages/rushing.py
+++ pages/rushing.py
@@ -18,7 +18,7 @@ season_to_analyze = st.sidebar.selectbox(
 
 # Web scraping data
 # Taken from the site:
-# https://www.pro-football-reference.com/years/2022/rushing.htm
+## https://www.pro-football-reference.com/years/2022/rushing.htm
 @st.cache
 def data_scrape(year):
     # Define the URL that's going to be used to search for the data.
@@ -30,7 +30,8 @@ def data_scrape(year):
     clean_data = rushing_df.drop(rushing_df[rushing_df.Age == 'Age'].index)
     clean_data = clean_data.fillna(0)
     rushing_players = clean_data.drop(['Rk'], axis=1)
-    rushing_players[["Age", "G", "GS", "Att", "Yds", "TD", "1D", "Lng", "Y/A", "Y/G", "Fmb"]] = rushing_players[["Age", "G", "GS", "Att", "Yds", "TD", "1D", "Lng", "Y/A", "Y/G", "Fmb"]].apply(pd.to_numeric)
+    rushing_players[["Age", "G", "GS", "Att", "Yds", "TD", "1D", "Lng", "Y/A", "Y/G", "Fmb"]] = rushing_players[[
+        "Age", "G", "GS", "Att", "Yds", "TD", "1D", "Lng", "Y/A", "Y/G", "Fmb"]].apply(pd.to_numeric)
     return rushing_players
 
 
@@ -57,13 +58,11 @@ age_filter = st.sidebar.multiselect(
 df_selected_team = player_stats[(player_stats['Tm'].isin(select_team)) & (
     player_stats['Pos'].isin(select_pos)) & (player_stats['Age'].isin(age_filter))]
 
-
 def download_csv(team_df):
     csv = team_df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="player_stats.csv">Download CSV File</a>'
     return href
-
 
 # Data displaying
 st.markdown("### Displaying player stats of the Selected team(s)")
@@ -78,10 +77,9 @@ def league_avg(player_df):
     temp_series = player_df.mean()
 
     temp_df = temp_df.append(temp_series, ignore_index=True)
-    temp_df.drop(labels=["Player","Tm","Pos"], inplace=True, axis=1)
+    temp_df.drop(labels=["Player", "Tm", "Pos"], inplace=True, axis=1)
     return temp_df
 
-# average_stats = get_averages(player_stats)
 
 def team_avg(player_df, team):
     temp_df = pd.DataFrame(columns=player_df.columns)
@@ -91,17 +89,30 @@ def team_avg(player_df, team):
     temp_series = temp_tm_df.mean()
 
     temp_df = temp_df.append(temp_series, ignore_index=True)
-    temp_df.drop(labels=["Player","Tm","Pos"], inplace=True, axis=1)
+    temp_df.drop(labels=["Player", "Tm", "Pos"], inplace=True, axis=1)
 
     return temp_df
 
+
+def player_information(player_df, player_name):
+    temp_df = pd.DataFrame(columns=player_df.columns)
+
+    temp_tm_df = pd.DataFrame(columns=player_df.columns)
+    temp_tm_df = player_df.loc[player_df['Player'] == player_name]
+
+    temp_df = temp_df.append(temp_tm_df, ignore_index=True)
+
+    temp_df.drop(labels=["Player", "Tm", "Pos"], inplace=True, axis=1)
+
+    return temp_df
 
 average_league_stats = league_avg(player_stats)
 
 # st.markdown("### Average statistics per team")
 # st.dataframe(average_stats)
 
-st.markdown(f"### Average statistics for the entire NFL in the {season_to_analyze} season")
+st.markdown(
+    f"### Average statistics for the entire NFL in the {season_to_analyze} season")
 st.dataframe(average_league_stats)
 
 if st.button("View League bargraph"):
@@ -113,6 +124,7 @@ if st.button("View League bargraph"):
     st.pyplot()
 
 
+# Team visualizations
 st.markdown("### Which team would you like to visualize?")
 
 raw_team_list = list(player_stats['Tm'].unique())
@@ -125,17 +137,49 @@ selected_team = st.selectbox(
 
 avg_team_stats = team_avg(player_stats, team=selected_team)
 
-st.markdown(f"### Average statistics for {selected_team}")
+st.markdown(f"#### Average statistics for {selected_team}")
 st.dataframe(avg_team_stats)
 
-# if st.button("View team bargraph"):
-tm_title = f"{selected_team} Average"
-st.markdown(f"#### Viewing stats for {selected_team}")
-# average_league_stats.to_csv('output.csv', index=False)
-# df=pd.read_csv('output.csv')
-plt.figure()
-sns.barplot(data=avg_team_stats).set(title=tm_title)
-st.pyplot()
+if st.button("View team bargraph"):
+    tm_title = f"{selected_team} Average"
+    st.markdown(f"#### Viewing stats for {selected_team}")
+
+    plt.figure()
+    sns.barplot(data=avg_team_stats).set(title=tm_title)
+    st.pyplot()
+
+# TODO: implement function to compare team stats and print if it was better, average or worse than the NFL's averages.
+
+# Player visualizations
+st.markdown("### Which player would you like to visualize?")
+
+raw_player_list = list(player_stats['Player'].unique())
+player_list = sorted(raw_player_list)
+
+selected_player = st.selectbox("Select a player", player_list)
+
+individual_player = player_information(player_df=player_stats, player_name=selected_player)
+
+st.markdown(f"#### Statistics for {selected_player}")
+st.dataframe(individual_player)
+
+if st.button(f"View {selected_player}'s stats"):
+    # tm_title = f"{selected_team} Average"
+    st.markdown(f"#### Viewing stats for {selected_player} against the NFL")
+
+    graph_title = f"Stats for {selected_player} compared with the NFL"
+
+    plt.figure()
+    sns.barplot(data=average_league_stats).set(title=graph_title)
+    sns.scatterplot(data=individual_player.transpose())
+    st.pyplot()
+
+st.markdown("""
+## About the data
+Some players have been in multiple teams throughout a season, that's why you may find some team names such as __2TM__.
+
+At the moment, the data from these players will be managed as if they had belonged to a team named "__2TM__" or whichever amount of teams they've been at in a single season.
+""")
 
 # Graphs
 # if st.button('View ')
