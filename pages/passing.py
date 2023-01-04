0e907e33420c9ce3f9a6a8d6ead16582d0ae8fcf
Daniel Roa <daniel.roa98@gmail.com>
2023-01-03 20:36:55 -0600

    Player analysis in Passing
    Started implementing the player analysis in passing.

--- pages/passing.py
+++ pages/passing.py
@@ -18,15 +18,19 @@ date = datetime.now()
 latest_season = int(date.strftime("%Y"))
 
 # Selecting a season to analyze
-season_to_analyze = st.sidebar.selectbox('Season', reversed(list(range(1980, latest_season))))
+season_to_analyze = st.sidebar.selectbox(
+    'Season', reversed(list(range(1980, latest_season))))
 
 st.sidebar.subheader("Sorting data")
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(['About', 'General stats', 'NFL Average', 'Team Average', 'Player Analysis'])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(
+    ['About', 'General stats', 'NFL Average', 'Team Average', 'Player Analysis'])
 
 # Web scraping function
 # Taken from:
 # https://www.pro-football-reference.com/years/{season}/passing.htm
+
+
 @st.cache(allow_output_mutation=True)
 def data_scrape(year: int):
     url = "https://www.pro-football-reference.com/years/" + \
@@ -66,13 +70,17 @@ selected_pos = st.sidebar.multiselect("Position", unique_pos, unique_pos)
 
 # Age filtering
 sort_player_ages = sorted(player_stats['Age'].unique())
-age_filter = st.sidebar.multiselect('Player age', sort_player_ages, sort_player_ages)
+age_filter = st.sidebar.multiselect(
+    'Player age', sort_player_ages, sort_player_ages)
 
 # Applying the data filter
-filtered_df = player_stats[(player_stats['Tm'].isin(selected_team)) & (player_stats['Pos'].isin(selected_pos)) & (player_stats['Age'].isin(age_filter))]
+filtered_df = player_stats[(player_stats['Tm'].isin(selected_team)) & (
+    player_stats['Pos'].isin(selected_pos)) & (player_stats['Age'].isin(age_filter))]
 
 # Function definitions
-def league_avg(player_df:pd.DataFrame):
+
+
+def league_avg(player_df: pd.DataFrame):
     temp_df = pd.DataFrame(columns=player_df.columns)
     temp_series = player_df.mean()
 
@@ -92,6 +100,19 @@ def team_avg(player_df, team):
     temp_df.drop(labels=["Player", "Tm", "Pos", "QBrec"], inplace=True, axis=1)
 
     return temp_df
+
+
+def player_information(player_df, player_name):
+    temp_df = pd.DataFrame(columns=player_df.columns)
+
+    temp_tm_df = pd.DataFrame(columns=player_df.columns)
+    temp_tm_df = player_df.loc[player_df["Player"] == player_name]
+
+    temp_df = temp_df.append(temp_tm_df, ignore_index=True)
+    temp_df.drop(labels=["Player", "Tm", "Pos"], inplace=True, axis=1)
+
+    return temp_df
+
 
 # Tabs
 # About
@@ -119,7 +140,8 @@ with tab2:
     st.markdown("### Displaying player stats with the selected filters")
     st.dataframe(filtered_df)
 
-    avg_t1,avg_t2,avg_t3 = st.tabs(["Best player in _Passing_", "Best player by position", '"Worst" player in _Passing_'])
+    avg_t1, avg_t2, avg_t3 = st.tabs(
+        ["Best player in _Passing_", "Best player by position", '"Worst" player in _Passing_'])
 
     with avg_t1:
         st.markdown("### Player with the most _Yards Gained by Passing_")
@@ -129,12 +151,14 @@ with tab2:
         player_name = best_player_yds["Player"]
         player_yds = best_player_yds["Yds"]
 
-        st.markdown(f"The best player is __{player_name}__ with __{player_yds}__ _Yards Gained by Passing_")
+        st.markdown(
+            f"The best player is __{player_name}__ with __{player_yds}__ _Yards Gained by Passing_")
         st.dataframe(best_player_yds, use_container_width=True)
 
         today = datetime.now()
         today_frmt = today.strftime("%d/%m/%Y")
-        st.markdown(f"This was updated in real time, so it means it updates at the same time as you view the data, just check the date 🗓️ {today_frmt}.")
+        st.markdown(
+            f"This was updated in real time, so it means it updates at the same time as you view the data, just check the date 🗓️ {today_frmt}.")
 
     with avg_t2:
         st.markdown(""" 
@@ -142,7 +166,6 @@ with tab2:
         
         Work in progress
         """)
-        
 
     with avg_t3:
         st.markdown('''
@@ -155,17 +178,18 @@ with tab2:
 with tab3:
     avg_nfl_stats = league_avg(player_df=player_stats)
 
-    st.markdown(f"### Passing average in the NFL in the {season_to_analyze} season")
+    st.markdown(
+        f"### Passing average in the NFL in the {season_to_analyze} season")
 
     st.dataframe(avg_nfl_stats, use_container_width=True)
 
     st.markdown(f"#### Graph of the NFL in the {season_to_analyze} season")
-
-    plot_title = f"League stats for the {season_to_analyze} season"
-    plt.figure()
-    sns.set(rc={'figure.figsize':(20,12)})
-    ax = sns.barplot(data=avg_nfl_stats).set(title=plot_title)
-    st.pyplot()
+    st.markdown("Pending minor fixes")
+    # plot_title = f"League stats for the {season_to_analyze} season"
+    # plt.figure()
+    # sns.set(rc={'figure.figsize':(20,12)})
+    # ax = sns.barplot(data=avg_nfl_stats).set(title=plot_title)
+    # st.pyplot()
 
 # Team averages
 with tab4:
@@ -174,7 +198,7 @@ with tab4:
     raw_team_list = list(player_stats["Tm"].unique())
     team_lst = sorted(raw_team_list)
 
-    selected_team = st.selectbox("Select a team from the box:",team_lst)
+    selected_team = st.selectbox("Select a team from the box:", team_lst)
 
     avg_tm_stats = team_avg(player_df=player_stats, team=selected_team)
 
@@ -183,10 +207,25 @@ with tab4:
 
     tm_title = f"{selected_team} average"
 
-    st.markdown(f"#### Viewing stats for {selected_team}")
-    plt.figure()
-    sns.barplot(data=avg_tm_stats).set(title=tm_title)
-    st.pyplot()
+    st.markdown("Pending minor fixes in the graph")
+
+    # st.markdown(f"#### Viewing stats for {selected_team}")
+    # plt.figure()
+    # sns.barplot(data=avg_tm_stats).set(title=tm_title)
+    # st.pyplot()
+
+with tab5:
+    st.markdown("### Which player would you like to visualize?")
+
+    raw_player_list = list(player_stats["Player"].unique())
+    player_list = sorted(raw_player_list)
+
+    selected_player = st.selectbox("Select a player", player_list)
+
+    player_info = player_information(player_df=player_stats, player_name=selected_player)
+
+    st.markdown(f"#### Statistics for {selected_player}")
+    st.dataframe(player_info, use_container_width=True)
 
 # Warning removal
 st.set_option('deprecation.showPyplotGlobalUse', False)
